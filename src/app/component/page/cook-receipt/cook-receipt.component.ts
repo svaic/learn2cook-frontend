a032c9f58261668dec85a6704f8b008c292092eb
Luka Jovanovski <luka.jovanovski@hotmail.com>
2022-09-08 03:12:26 +0200

    cook-receipt set receipt to observable<receipt>

--- src/app/component/page/cook-receipt/cook-receipt.component.ts
+++ src/app/component/page/cook-receipt/cook-receipt.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Step} from "../../../model/Step";
 import {Receipt} from "../../../model/Receipt";
 import {ReceiptService} from "../../../service/receipt/receipt.service";
 import {ActivatedRoute} from "@angular/router";
 import {Store} from "@ngrx/store";
 import {mapDifficulty, parseDuration, ReceiptDifficultyData} from "../../../utility/utility";
+import {Observable, switchMap} from "rxjs";
+import {ReceiptDifficulty} from "../../../model/enumerable/ReceiptDifficulty";
 
 @Component({
   selector: 'app-cook-receipt',
@@ -13,33 +15,36 @@ import {mapDifficulty, parseDuration, ReceiptDifficultyData} from "../../../util
 })
 export class CookReceiptComponent implements OnInit {
 
-  receipt!: Receipt;
+  receipt$: Observable<Receipt>;
   currentStep: Step | undefined;
+  steps: Step[] | undefined;
 
   constructor(private receiptService: ReceiptService, private route: ActivatedRoute, private readonly store: Store) {
 
-    this.route.params.subscribe(params => {
-      receiptService.getReceipt(params['id'] as number)
-        .subscribe(x => this.receipt = x);
-    });
+    this.receipt$ = this.route.params.pipe(switchMap(params => {
+      return receiptService.getReceipt(params['id'] as number)
+    }));
   }
 
   startReceipt() {
-    this.currentStep = this.receipt.steps[0];
+    this.receipt$.subscribe(receipt => {
+      this.currentStep = receipt.steps[0];
+      this.steps = receipt.steps;
+    })
   }
 
   nextStep() {
     let nextStep = this.currentStep!.stepNumber + 1;
-    this.currentStep = this.receipt.steps[nextStep-1];
+    this.currentStep = this.steps![nextStep - 1];
   }
 
   previousStep() {
     let prevStep = this.currentStep!.stepNumber - 1;
-    this.currentStep = this.receipt.steps[prevStep-1];
+    this.currentStep = this.steps![prevStep - 1];
   }
 
-  getDifficulty(): ReceiptDifficultyData {
-    return mapDifficulty[this.receipt.difficulty];
+  getDifficulty(difficulty: ReceiptDifficulty): ReceiptDifficultyData {
+    return mapDifficulty[difficulty];
   }
 
   getDuration(duration: string) {
